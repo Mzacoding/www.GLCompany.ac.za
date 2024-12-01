1f169ca936fae6fc1c1a76088bb69f53c77cbc57
Nyiko Mkansi <134933325+Mzacoding@users.noreply.github.com>
2024-12-01 16:10:12 +0200

    Add files via upload

---
+++ GLCampany.js
@@ -0,0 +1,14 @@
+ 
+function Showsidebar(){
+
+    const sidebar=document.querySelector('.sidebar');
+    sidebar.style.display='flex';
+}
+
+function Hidesidebar(){
+
+    const sidebar=document.querySelector('.sidebar');
+    sidebar.style.display='none';
+}
+
+ 
